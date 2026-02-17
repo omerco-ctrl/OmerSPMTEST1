a66b54406f5169f98cc36d5edcda9048de8b6bca
Omer Cohen <omer.co@appcharge.com>
2026-02-17 13:10:40 +0200

    Release 5.0.2 (SPM)

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ACPaymentLinks",
-            url: "https://github.com/omerco-ctrl/OmerSPMTEST1/releases/download/5.0.1/ACPaymentLinks.xcframework.zip",
-            checksum: "e227a0114f4248030ade30bec8f9a81f61a6fad1288fb8381441ac640bfa2067"
+            url: "https://github.com/omerco-ctrl/OmerSPMTEST1/releases/download/5.0.2/ACPaymentLinks.xcframework.zip",
+            checksum: "e9c23979b809bf6147a13edda372d391a3460cc62d5311cb456e1ffe85f6fcd0"
         )
     ]
 )

7acc18715e27b368a91c2753fd9f1631e9d2033e
Omer Cohen <omer.co@appcharge.com>
2026-02-17 11:38:19 +0200

    Release 1.0.1 (SPM)

--- Package.swift
+++ Package.swift
@@ -1,29 +1,4 @@
 // swift-tools-version: 5.9
-//
-//  Package.swift
-//
-//  Copyright (c) 2025 Appcharge (https://www.appcharge.com/)
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
-//
-
-// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
@@ -40,11 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ACPaymentLinks",
-            url: "https://github.com/omerco-ctrl/OmerSPMTEST1/releases/download/1.0.0/ACPaymentLinks.xcframework.zip",
-            checksum: "111bd59a819103c0784e31f60a41c8c37c84add6bf603e767738c4ee7817f87d"
+            url: "https://github.com/omerco-ctrl/OmerSPMTEST1/releases/download/1.0.1/ACPaymentLinks.xcframework.zip",
+            checksum: "1bdf16c8eb7259eafc4a62e1be2263a37cc2f3f928fdbbddcacd1b588df0f592"
         )
     ]
 )
-
-
-
